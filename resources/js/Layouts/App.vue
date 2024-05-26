80b788c50e33c5e1ce4d664c0a4206a09148ccdb
ChamodMullegama <chamod.mullegama@gmail.com>
2024-05-27 01:43:48 +0530

    first commit

---
+++ resources/js/Layouts/App.vue
@@ -0,0 +1,27 @@
+<template>
+        <Side />
+
+    <div class="main-content" id="panel">
+        <Nav />
+
+        <slot name="content" />
+
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Nav from "@/Components/Main/Nav.vue";
+import Side from "@/Components/Main/Side.vue";
+import Footer from "@/Components/Main/Footer.vue";
+
+export default {
+    components: {
+        Nav,
+        Side,
+        Footer,
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ resources/js/app.js
@@ -0,0 +1,41 @@
+import "./bootstrap";
+import "../css/app.css";
+import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js";
+
+import "../src/vendor/jquery/dist/jquery.min.js";
+import "../src/vendor/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../src/vendor/js-cookie/js.cookie.js";
+// import "../src/vendor/jquery.scrollbar/jquery.scrollbar.min.js";
+//  import "../src/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js";
+
+import "../src/vendor/chart.js/dist/Chart.min.js";
+import "../src/vendor/chart.js/dist/Chart.extension.js";
+//  import "../src/js/argon.min.js?v=1.2.1";
+import "../src/js/demo.min.js";
+
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy";
+
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob("./Pages/**/*.vue")
+        ),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: "#4B5563",
+    },
+});
+
+
